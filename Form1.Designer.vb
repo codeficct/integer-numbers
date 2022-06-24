ded1aa0732b6a4b6d21032d121be483adf4905b3
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-23 21:47:17 -0400

    feat(Root): Setup app, notes

---
+++ Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrimeNumbersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Result = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Hack Nerd Font", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(369, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Integer Numbers"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(386, 83)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(914, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrimeNumbersToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'PrimeNumbersToolStripMenuItem
+        '
+        Me.PrimeNumbersToolStripMenuItem.Name = "PrimeNumbersToolStripMenuItem"
+        Me.PrimeNumbersToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PrimeNumbersToolStripMenuItem.Text = "Prime Numbers"
+        '
+        'Result
+        '
+        Me.Result.AutoSize = True
+        Me.Result.Location = New System.Drawing.Point(396, 159)
+        Me.Result.Name = "Result"
+        Me.Result.Size = New System.Drawing.Size(79, 15)
+        Me.Result.TabIndex = 3
+        Me.Result.Text = "Result..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(914, 450)
+        Me.Controls.Add(Me.Result)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Hack Nerd Font", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PrimeNumbersToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Result As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    ' Check if is prime number
+    Public Function isPrime(ByVal number As Integer) As Boolean
+        If number > 1 Then
+            For i As Integer = 2 To number - 1
+                If number Mod i = 0 Then
+                    Return False
+                End If
+            Next
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function CheckIfItIsFibonacci(n As Integer) As Boolean
+        Dim x, y, z As Integer
+        x = -1 : y = 0
+        Do While (z = n) Or (z < n)
+            z = x + y
+            x = y : y = z
+        Loop
+        Return z
+    End Function
+
+    Private Sub PrimeNumbersToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrimeNumbersToolStripMenuItem.Click
+        Result.Text = isPrime(TextBox1.Text)
+    End Sub
+End Class
