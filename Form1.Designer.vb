8c554b7a41a39bf1aeb29dfd58b3e27be33bc1cf
Luis Gabriel Janco <janco7249@gmail.com>
2022-06-30 18:45:12 -0400

    feat: access number examples

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -27,7 +27,15 @@ Partial Class Form1
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PrimeNumbersToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Menu2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AccesNumberToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Sum2DigitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Sum3DigitsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Sum4DigitsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReverseNumberToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OnlyEvenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Result = New System.Windows.Forms.Label()
+        Me.OnlyDoddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -35,7 +43,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Hack Nerd Font", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(369, 27)
+        Me.Label1.Location = New System.Drawing.Point(229, 128)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(127, 15)
         Me.Label1.TabIndex = 0
@@ -43,17 +51,17 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(386, 83)
+        Me.TextBox1.Location = New System.Drawing.Point(246, 184)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 23)
         Me.TextBox1.TabIndex = 1
         '
         'MenuStrip1
         '
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.Menu2ToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(914, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(573, 24)
         Me.MenuStrip1.TabIndex = 2
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -67,23 +75,72 @@ Partial Class Form1
         'PrimeNumbersToolStripMenuItem
         '
         Me.PrimeNumbersToolStripMenuItem.Name = "PrimeNumbersToolStripMenuItem"
-        Me.PrimeNumbersToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PrimeNumbersToolStripMenuItem.Size = New System.Drawing.Size(157, 22)
         Me.PrimeNumbersToolStripMenuItem.Text = "Prime Numbers"
+        '
+        'Menu2ToolStripMenuItem
+        '
+        Me.Menu2ToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AccesNumberToolStripMenuItem, Me.Sum2DigitToolStripMenuItem, Me.Sum3DigitsToolStripMenuItem, Me.Sum4DigitsToolStripMenuItem, Me.ReverseNumberToolStripMenuItem, Me.OnlyEvenToolStripMenuItem, Me.OnlyDoddToolStripMenuItem})
+        Me.Menu2ToolStripMenuItem.Name = "Menu2ToolStripMenuItem"
+        Me.Menu2ToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
+        Me.Menu2ToolStripMenuItem.Text = "Acces Number"
+        '
+        'AccesNumberToolStripMenuItem
+        '
+        Me.AccesNumberToolStripMenuItem.Name = "AccesNumberToolStripMenuItem"
+        Me.AccesNumberToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AccesNumberToolStripMenuItem.Text = "Sum 1 digits"
+        '
+        'Sum2DigitToolStripMenuItem
+        '
+        Me.Sum2DigitToolStripMenuItem.Name = "Sum2DigitToolStripMenuItem"
+        Me.Sum2DigitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Sum2DigitToolStripMenuItem.Text = "Sum 2 digits"
+        '
+        'Sum3DigitsToolStripMenuItem
+        '
+        Me.Sum3DigitsToolStripMenuItem.Name = "Sum3DigitsToolStripMenuItem"
+        Me.Sum3DigitsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Sum3DigitsToolStripMenuItem.Text = "Sum 3 digits"
+        '
+        'Sum4DigitsToolStripMenuItem
+        '
+        Me.Sum4DigitsToolStripMenuItem.Name = "Sum4DigitsToolStripMenuItem"
+        Me.Sum4DigitsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.Sum4DigitsToolStripMenuItem.Text = "Sum 4 digits"
+        '
+        'ReverseNumberToolStripMenuItem
+        '
+        Me.ReverseNumberToolStripMenuItem.Name = "ReverseNumberToolStripMenuItem"
+        Me.ReverseNumberToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ReverseNumberToolStripMenuItem.Text = "Verify if is capicua"
+        '
+        'OnlyEvenToolStripMenuItem
+        '
+        Me.OnlyEvenToolStripMenuItem.Name = "OnlyEvenToolStripMenuItem"
+        Me.OnlyEvenToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OnlyEvenToolStripMenuItem.Text = "Only Even"
         '
         'Result
         '
         Me.Result.AutoSize = True
-        Me.Result.Location = New System.Drawing.Point(396, 159)
+        Me.Result.Location = New System.Drawing.Point(256, 260)
         Me.Result.Name = "Result"
         Me.Result.Size = New System.Drawing.Size(79, 15)
         Me.Result.TabIndex = 3
         Me.Result.Text = "Result..."
         '
+        'OnlyDoddToolStripMenuItem
+        '
+        Me.OnlyDoddToolStripMenuItem.Name = "OnlyDoddToolStripMenuItem"
+        Me.OnlyDoddToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OnlyDoddToolStripMenuItem.Text = "Only Dodd"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(914, 450)
+        Me.ClientSize = New System.Drawing.Size(573, 450)
         Me.Controls.Add(Me.Result)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Label1)
@@ -105,4 +162,12 @@ Partial Class Form1
     Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents PrimeNumbersToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents Result As Label
+    Friend WithEvents Menu2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccesNumberToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Sum2DigitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Sum3DigitsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Sum4DigitsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReverseNumberToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OnlyEvenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OnlyDoddToolStripMenuItem As ToolStripMenuItem
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,8 @@
 ﻿Public Class Form1
+    Dim messageError As String = "Please enter a valid number"
+    Public Function EvenAndOdd(num As Integer) As Boolean
+        Return num Mod 2 = 0
+    End Function
     ' Check if is prime number
     Public Function isPrime(ByVal number As Integer) As Boolean
         If number > 1 Then
@@ -23,7 +27,142 @@
         Return z
     End Function
 
+    Public Function Factorial(number As Double) As Double
+        Dim result As Double
+        Dim index As Integer
+        If number <> 0 Then
+            result = 1
+            For index = Math.Abs(number) To 1 Step -1
+                result *= index
+            Next
+            If number < 0 Then
+                result = -result
+            End If
+        Else
+            result = 1
+        End If
+        Return result
+    End Function
+
+    Public Function AccessNumber(number As Integer, Digits As Integer) As Integer
+        Dim sum, digit, divider As Integer
+        Select Case Digits
+            Case 1
+                divider = 10
+            Case 2
+                divider = 100
+            Case 3
+                divider = 1000
+            Case 4
+                divider = 10000
+        End Select
+        While number > 0
+            digit = number Mod divider
+            sum += digit
+            number \= divider
+        End While
+        Return sum
+    End Function
+
+    Public Function ReverseNumber(number As Integer) As Integer
+        Dim result, digit As Integer
+        result = 0
+        While number > 0
+            digit = number Mod 10
+            result = result * 10 + digit
+            number \= 10
+        End While
+        Return result
+    End Function
+
+    Public Function OnlyEvenOrOdd(number As Integer, isEven As Boolean) As Integer
+        Dim digit, result As Integer
+        While number > 0
+            digit = number Mod 10
+            If EvenAndOdd(digit) And isEven Then
+                result = result * 10 + digit
+            ElseIf Not EvenAndOdd(digit) And Not isEven Then
+                result = result * 10 + digit
+            End If
+            number \= 10
+        End While
+        Return ReverseNumber(result)
+    End Function
+
+    Public Function BinaryToDecimal(binary As Integer) As Integer
+        Dim result, digit As Integer
+        result = 0
+        For index = 0 To binary
+
+        Next
+        Return result
+    End Function
+
+    Public Function CheckMatch(number As Integer) As Boolean
+        Return ReverseNumber(number) = number
+    End Function
+
     Private Sub PrimeNumbersToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrimeNumbersToolStripMenuItem.Click
-        Result.Text = isPrime(TextBox1.Text)
+        Try
+            Result.Text = isPrime(TextBox1.Text)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub AccesNumberToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AccesNumberToolStripMenuItem.Click
+        Try
+            Result.Text = AccessNumber(TextBox1.Text, 1)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub Sum2DigitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Sum2DigitToolStripMenuItem.Click
+        Try
+            Result.Text = AccessNumber(TextBox1.Text, 2)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub Sum3DigitsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Sum3DigitsToolStripMenuItem.Click
+        Try
+            Result.Text = AccessNumber(TextBox1.Text, 3)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub Sum4DigitsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Sum4DigitsToolStripMenuItem.Click
+        Try
+            Result.Text = AccessNumber(TextBox1.Text, 4)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub ReverseNumberToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReverseNumberToolStripMenuItem.Click
+        Try
+            Result.Text = CheckMatch(TextBox1.Text)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub OnlyEvenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OnlyEvenToolStripMenuItem.Click
+        Try
+            Result.Text = OnlyEvenOrOdd(TextBox1.Text, true)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
+    End Sub
+
+    Private Sub OnlyDoddToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OnlyDoddToolStripMenuItem.Click
+        Try
+            Result.Text = OnlyEvenOrOdd(TextBox1.Text, False)
+        Catch ex As Exception
+            MsgBox(messageError)
+        End Try
     End Sub
 End Class
